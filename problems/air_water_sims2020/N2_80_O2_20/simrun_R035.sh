2ac5e50116dd867a0ec965831a9129a01bf65766
Shane <keniley1@illinois.edu>
2020-10-15 15:39:18 -0500

    Updating air water simulation files for Lemhi

---
+++ problems/air_water_sims2020/N2_80_O2_20/simrun_R035.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -M keniley1@illinois.edu
+#PBS -m abe
+#PBS -N air_R035_01
+#PBS -P moose
+#PBS -l select=1:ncpus=12:mpiprocs=12
+#PBS -l walltime=24:00:00
+
+cd $PBS_O_WORKDIR
+cat $PBS_NODEFILE
+
+#module load use.moose PETSc
+module load use.moose PETSc/3.11.4-GCC
+MV2_ENABLE_AFFINITY=0 mpiexec ~/projects/lemhi/zapdos/zapdos-opt -i sim_R035.i --color off >> sim_R035_terminal_01
