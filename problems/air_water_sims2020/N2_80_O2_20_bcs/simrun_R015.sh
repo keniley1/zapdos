555376b2ad27f76fdd857423115c4f33f89af0a9
Shane <keniley1@illinois.edu>
2020-10-16 14:12:42 -0500

    Adding more input files

---
+++ problems/air_water_sims2020/N2_80_O2_20_bcs/simrun_R015.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -M keniley1@illinois.edu
+#PBS -m abe
+#PBS -N air_R015_01
+#PBS -P moose
+#PBS -l select=1:ncpus=12:mpiprocs=12
+#PBS -l walltime=24:00:00
+
+cd $PBS_O_WORKDIR
+cat $PBS_NODEFILE
+
+#module load use.moose PETSc
+module load use.moose PETSc/3.11.4-GCC
+MV2_ENABLE_AFFINITY=0 mpiexec ~/projects/lemhi/zapdos/zapdos-opt -i sim_R015.i --color off >> sim_R015_terminal_01
