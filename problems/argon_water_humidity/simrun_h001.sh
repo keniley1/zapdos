7536fff52ba3ac5efe9997f45e4c26e461d0f7c8
Shane Keniley <keniley1@illinois.edu>
2021-02-07 14:08:06 -0600

    Fixing output number in shell scripts

--- problems/argon_water_humidity/simrun_h001.sh
+++ problems/argon_water_humidity/simrun_h001.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -M keniley1@illinois.edu
 #PBS -m abe
-#PBS -N Ar_001_humid_01
+#PBS -N Ar_001_humid_02
 #PBS -P moose
 #PBS -l select=1:ncpus=12:mpiprocs=12
 #PBS -l walltime=06:00:00
@@ -11,4 +11,4 @@ cat $PBS_NODEFILE
 
 #module load use.moose PETSc
 module load use.moose PETSc/3.11.4-GCC
-MV2_ENABLE_AFFINITY=0 mpiexec ~/projects/lemhi/zapdos/zapdos-opt -i sim_h001.i --color off >> stdout_h001_01
+MV2_ENABLE_AFFINITY=0 mpiexec ~/projects/lemhi/zapdos/zapdos-opt -i sim_h001.i --color off >> stdout_h001_02
