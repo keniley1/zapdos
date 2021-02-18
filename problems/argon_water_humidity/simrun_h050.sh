93b1e5f8aaa2f6a53118587d0b1b87a406925114
Shane Keniley <keniley1@illinois.edu>
2021-02-18 17:14:43 -0600

    Updating input files again

---
+++ problems/argon_water_humidity/simrun_h050.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -M keniley1@illinois.edu
+#PBS -m abe
+#PBS -N Ar_050_humid_01
+#PBS -P moose
+#PBS -l select=1:ncpus=12:mpiprocs=12
+#PBS -l walltime=12:00:00
+
+cd $PBS_O_WORKDIR
+cat $PBS_NODEFILE
+
+#module load use.moose PETSc
+module load use.moose PETSc/3.11.4-GCC
+MV2_ENABLE_AFFINITY=0 mpiexec ~/projects/lemhi/zapdos/zapdos-opt -i sim_h050.i --color off >> stdout_h050_01
