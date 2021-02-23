64b7c2924d6b84e4eb61a309fd82078cbe708f72
Shane <keniley1@illinois.edu>
2021-02-22 18:56:02 -0600

    Updating files besides h020

--- problems/argon_water_humidity/simrun_h065.sh
+++ problems/argon_water_humidity/simrun_h065.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -M keniley1@illinois.edu
 #PBS -m abe
-#PBS -N Ar_065_humid_02
+#PBS -N Ar_065_humid_03
 #PBS -P moose
 #PBS -l select=1:ncpus=12:mpiprocs=12
 #PBS -l walltime=08:00:00
@@ -11,4 +11,4 @@ cat $PBS_NODEFILE
 
 #module load use.moose PETSc
 module load use.moose PETSc/3.11.4-GCC
-MV2_ENABLE_AFFINITY=0 mpiexec ~/projects/lemhi/zapdos/zapdos-opt -i sim_h065.i --color off >> stdout_h065_02
+MV2_ENABLE_AFFINITY=0 mpiexec ~/projects/lemhi/zapdos/zapdos-opt -i sim_h065.i --color off >> stdout_h065_03
